758abf95fe299b1368eb89465513d968e962316e
Pete Alberico <palberico@yahoo.com>
2024-03-04 18:42:44 -0700

    netlify

--- src/views/Home.js
+++ src/views/Home.js
@@ -12,28 +12,28 @@ import Vacation from '../assets/images/ducks/Vacation.png';
 import Zebra from '../assets/images/ducks/Zebra.png';
 
 const ducks = [
-  { name: "Lando", image: Lion, footerColor: 'linear-gradient(to right, #ff8000, #47C7FC, #ffffff, #000000)' }, // Example gradient from orange to a lighter orange
-  { name: "Oscar", image: Elly, footerColor: 'linear-gradient(to right, #ff8000, #47C7FC, #ffffff, #000000)' }, // Gradient from red to a slightly different shade of red
-  { name: "Carlos", image: Rich, footerColor: 'linear-gradient(to right, #EF1A2D, #FFF200, #000000, #00A551)' },
-  { name: "Charles", image: Mine, footerColor: 'linear-gradient(to right, #EF1A2D, #FFF200, #000000, #00A551)' },
-  { name: "Max", image: Monster, footerColor: 'linear-gradient(to right, #C0BFBF, #E30118, #FDD900)' },
-  { name: "Sergio", image: Zebra, footerColor: 'linear-gradient(to right, #C0BFBF, #E30118, #FDD900)' },
-  { name: "Lewis", image: Ninja, footerColor: 'linear-gradient(to right, #000000, #C8CCCE, #00A19B, #565F64)' },
-  { name: "George", image: Vacation, footerColor: 'linear-gradient(to right, #000000, #C8CCCE, #00A19B, #565F64)' },
+  { name: "Lando", image: Lion, footerColor: 'linear-gradient(to right, #ff8000, #47C7FC, #000000)' }, // Example gradient from orange to a lighter orange
+  { name: "Oscar", image: Elly, footerColor: 'linear-gradient(to right, #ff8000, #47C7FC, #000000)' }, // Gradient from red to a slightly different shade of red
+  { name: "Carlos", image: Rich, footerColor: 'linear-gradient(to right, #EF1A2D, #000000)' },
+  { name: "Charles", image: Mine, footerColor: 'linear-gradient(to right, #EF1A2D, #000000)' },
+  { name: "Max", image: Monster, footerColor: 'linear-gradient(to right, #E30118, #FDD900)' },
+  { name: "Sergio", image: Zebra, footerColor: 'linear-gradient(to right, #E30118, #FDD900)' },
+  { name: "Lewis", image: Ninja, footerColor: 'linear-gradient(to right, #000000, #00A19B, #565F64)' },
+  { name: "George", image: Vacation, footerColor: 'linear-gradient(to right, #000000, #00A19B, #565F64)' },
 ];
 
 
 const TempHome = () => {
   return (
-    <div key={duck.name} style={{ backgroundImage: duck.footerColor }}>
-      {ducks.map((duck, index) => (
+    <div key={ducks.name} style={{ backgroundImage: ducks.footerColor }}>
+      {ducks.map((ducks, index) => (
         <Link to="/">
         <Card key={index}>
-          <Image src={duck.image} wrapped ui={false} />
+          <Image src={ducks.image} wrapped ui={false} />
           <Card.Content>
-            <Card.Header style={{ textAlign: 'center' }}>{duck.name}</Card.Header>
+            <Card.Header style={{ textAlign: 'center' }}>{ducks.name}</Card.Header>
           </Card.Content>
-          <div style={{ ...styles.footer, backgroundColor: duck.footerColor }}></div>
+          <div style={{ ...styles.footer, backgroundImage: ducks.footerColor }}></div>
         </Card>
         </Link>
       ))}
