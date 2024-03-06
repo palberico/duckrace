85590728cdfdafaa17e47cc1410b56db207bbacc
Peter Alberico <palberico@yahoo.com>
2024-03-06 16:56:19 -0700

    logo fixes

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import DuckCard from '../components/DuckCard';
 import LeaderBoard from '../components/LeaderBoard';
-import '../App.css'; 
+import { Input, Menu, Image } from 'semantic-ui-react';
+import logo from '../assets/images/Logo.png'; // Adjust the path if necessary
+import '../App.css'; // Ensure this import is present
 
 const Home = () => {
   return (
-    <div className="homeContainer">
-      <LeaderBoard />
-      <DuckCard />
+    <div>
+      <div className="header">
+        <div className="headerLogo">
+          <Image src={logo} size='small' />
+        </div>
+        <div className="headerSearch">
+          <Input icon='search' placeholder='Search...' fluid />
+        </div>
+      </div>
+      <div className="homeContainer">
+        <LeaderBoard />
+        <DuckCard />
+      </div>
     </div>
   );
 };
 
 export default Home;
-
-
