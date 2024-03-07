dd4685d6ee4e5282a37c190628da0415d58a2429
Pete Alberico <palberico@yahoo.com>
2024-03-06 22:48:11 -0700

    netlify

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Input, Image, Button, Message } from 'semantic-ui-react';
 import { useNavigate } from 'react-router-dom';
 import { db } from '../firebase/Config';
