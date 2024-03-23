f6f0988a63e7dbb9f58d524371cb91c1591bb833
Pete Alberico <palberico@yahoo.com>
2024-03-23 14:47:02 -0600

    api changes

--- src/components/data/Countries.js
+++ src/components/data/Countries.js
@@ -5,14 +5,17 @@ const countryOptions = [
     { key: 'ca', value: 'Canada', text: 'Canada' },
     
     { key: 'ag', value: 'AG', text: 'Antigua and Barbuda' },
+    { key: 'ai', value: 'AI', text: 'Anguilla' },
     { key: 'aw', value: 'AW', text: 'Aruba' },
     { key: 'bs', value: 'BS', text: 'Bahamas' },
     { key: 'bb', value: 'BB', text: 'Barbados' },
     { key: 'bz', value: 'BZ', text: 'Belize' },
     { key: 'bm', value: 'BM', text: 'Bermuda' },
     { key: 'vg', value: 'VG', text: 'British Virgin Islands' },
+    { key: 'bq', value: 'BQ', text: 'Bonaire' },
     { key: 'ky', value: 'KY', text: 'Cayman Islands' },
     { key: 'cu', value: 'CU', text: 'Cuba' },
+    { key: 'cw', value: 'CW', text: 'Curaçao' },
     { key: 'dm', value: 'DM', text: 'Dominica' },
     { key: 'do', value: 'DO', text: 'Dominican Republic' },
     { key: 'gd', value: 'GD', text: 'Grenada' },
@@ -22,8 +25,6 @@ const countryOptions = [
     { key: 'jm', value: 'JM', text: 'Jamaica' },
     { key: 'mq', value: 'MQ', text: 'Martinique' },
     { key: 'ms', value: 'MS', text: 'Montserrat' },
-    { key: 'cw', value: 'CW', text: 'Curaçao' },
-    { key: 'bq', value: 'BQ', text: 'Bonaire' },
     { key: 'ni', value: 'NI', text: 'Nicaragua' },
     { key: 'pa', value: 'PA', text: 'Panama' },
     { key: 'pr', value: 'PR', text: 'Puerto Rico' },
@@ -33,13 +34,11 @@ const countryOptions = [
     { key: 'mf', value: 'MF', text: 'Saint Martin' },
     { key: 'pm', value: 'PM', text: 'Saint Pierre and Miquelon' },
     { key: 'vc', value: 'VC', text: 'Saint Vincent and the Grenadines' },
-    { key: 'sx', value: 'SX', text: 'Sint Maarten' },
+    { key: 'sx', value: 'SX', text: 'Saint Maarten' },
     { key: 'tt', value: 'TT', text: 'Trinidad and Tobago' },
     { key: 'tc', value: 'TC', text: 'Turks and Caicos Islands' },
     { key: 'vi', value: 'VI', text: 'United States Virgin Islands' },
-    { key: 'vg', value: 'VG', text: 'Virgin Islands, British' },
-    { key: 'ai', value: 'AI', text: 'Anguilla' },
-    { key: 'vg', value: 'VG', text: 'Virgin Islands (U.S.)' }
+
   ];
   
   export default countryOptions;
--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -3,14 +3,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDd5MdJGZC30amPs7_vb7v9EyFvIX8rD-c",
-    authDomain: "raceducks.firebaseapp.com",
-    projectId: "raceducks",
-    storageBucket: "raceducks.appspot.com",
-    messagingSenderId: "26834943334",
-    appId: "1:26834943334:web:bd0c427f3ad92e8b00dca7",
-    measurementId: "G-S5KRXVSGR4"
-  };
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -19,4 +19,3 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 
 export { db, storage };
-
