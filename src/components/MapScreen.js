de4fb36c451157c3abe3fa7b2189541a7e92ed36
Pete Alberico <palberico@yahoo.com>
2024-04-04 21:37:32 -0600

    fixed map locations

--- src/components/MapScreen.js
+++ src/components/MapScreen.js
@@ -1,20 +1,34 @@
-
 import React, { useEffect, useRef } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase/Config'; // Ensure this path is correct
 
 const MapScreen = () => {
   const mapRef = useRef(null);
   const defaultPosition = [51.505, -0.09]; // Default coordinates for the map's initial view
 
   useEffect(() => {
-    // Initialize the map
     const map = L.map(mapRef.current).setView(defaultPosition, 13);
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© OpenStreetMap contributors',
     }).addTo(map);
 
-    // Cleanup function to remove the map when the component unmounts
+    // Fetch locations from Firestore
+    const fetchLocations = async () => {
+      const querySnapshot = await getDocs(collection(db, "locations"));
+      querySnapshot.forEach((doc) => {
+        const locationData = doc.data().startLocation;
+        const coordinates = [locationData.coordinates.latitude, locationData.coordinates.longitude];
+        L.marker(coordinates)
+          .addTo(map)
+          .bindPopup(`Location: ${locationData.city}, ${locationData.state}<br>Visited on: ${new Date(doc.data().timestamp.seconds * 1000).toDateString()}`);
+      });
+    };
+
+    fetchLocations();
+
     return () => {
       map.remove();
     };
